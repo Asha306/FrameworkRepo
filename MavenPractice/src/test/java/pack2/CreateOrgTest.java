d56288791b143be5d5850e3fb62bf117bd0a2f58
Asha306 <reddyasha.21@gmail.com>
2024-11-12 17:10:02 +0530

    first push

---
+++ MavenPractice/src/test/java/pack2/CreateOrgTest.java
@@ -0,0 +1,17 @@
+package pack2;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+@Test
+
+public void createOrgTest() {
+	System.out.println("createOrgTest executed");
+}
+@Test
+public void modifyOrgTest() {
+	
+	System.out.println("modifyOrgTest executed");	
+	
+}
+}
