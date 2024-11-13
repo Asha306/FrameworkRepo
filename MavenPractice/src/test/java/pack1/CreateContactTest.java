65bfcb6ddb2c72fe43d6df0ff78539739a39e322
Asha306 <reddyasha.21@gmail.com>
2024-11-13 11:13:23 +0530

    delete the one testscript

--- MavenPractice/src/test/java/pack1/CreateContactTest.java
+++ MavenPractice/src/test/java/pack1/CreateContactTest.java
@@ -3,33 +3,23 @@ package pack1;
 import org.testng.annotations.Test;
 
 public class CreateContactTest {
-@Test
-public void createContactTest() {
-	System.out.println("createContactTest executed");
-	String URL = System.getProperty("url");
-	String BROWSER = System.getProperty("browser");
-	String USERNAME = System.getProperty("username");
-	String PASSWORD = System.getProperty("password");
-	System.out.println(URL);
-	System.out.println(BROWSER);
-	System.out.println(USERNAME);
-	System.out.println(PASSWORD);
-}
-@Test
-public void modifyContactTest() {
-System.out.println("modifyContact executed");	
-	
-	
-	
-}
+	@Test
+	public void createContactTest() {
+		System.out.println("createContactTest executed");
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+	}
 
-@Test
-public void deleteContactTest() {
-System.out.println("deleteContact executed");	
-	
-	
-	
-}
+	@Test
+	public void modifyContactTest() {
+		System.out.println("modifyContact executed");
+
+	}
 
 }
-
